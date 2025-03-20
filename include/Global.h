72ea4033ddfe25313be2d82a6f53cb88050f772f
Sean Im <92773124+seanim1@users.noreply.github.com>
2025-03-20 17:53:10 +0900

    completed Windows_Specific

--- include/Global.h
+++ include/Global.h
@@ -1,2 +1,16 @@
 #pragma once
 #include <iostream>
+typedef struct CPU_CAPABILITIES {
+	uint8_t is_SMT_enabled;
+	uint8_t logicalThreadCount;
+	uint8_t physicalCoreCount;
+	uint16_t L1cacheLineSize_Byte;
+	uint32_t L1cacheSize_Byte;
+	uint32_t L2cacheSize_Byte;
+	uint32_t L3cacheSize_Byte;
+} CPU_CAPABILITIES;
+extern CPU_CAPABILITIES cpuCap;
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define FILE_AND_LINE __FILE__ ": Line " TOSTRING(__LINE__)
--- include/Windows_Specific.h
+++ include/Windows_Specific.h
@@ -1,3 +1,18 @@
 #pragma once
+
 #include "Global.h"
+#include <windows.h>
 void queryProcessor();
+#define CATCH_ERROR(result)                                    \
+if (result < 0) {                                                               \
+    MessageBoxA(NULL, FILE_AND_LINE, "Error!", MB_ICONEXCLAMATION | MB_OK);   \
+}
+#define CATCH_ZERO_ERROR(result)                                    \
+if (result == 0) {                                                               \
+    MessageBoxA(NULL, FILE_AND_LINE, "Error (False)!", MB_ICONEXCLAMATION | MB_OK);   \
+}
+#define CATCH_MALLOC_ERROR(ptr)                                  \
+if (ptr == NULL) {                                          \
+    MessageBoxA(NULL, FILE_AND_LINE, "Malloc Failed!", MB_ICONEXCLAMATION | MB_OK); \
+    exit(EXIT_FAILURE);                                      \
+}
