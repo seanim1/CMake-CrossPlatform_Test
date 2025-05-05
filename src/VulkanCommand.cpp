0d1029ca2d07f55779e864cf1a88c9867465e690
Sean Im <92773124+seanim1@users.noreply.github.com>
2025-05-05 02:31:10 -0700

    ignore

--- src/VulkanCommand.cpp
+++ src/VulkanCommand.cpp
@@ -98,6 +98,8 @@ void VulkanCommand::buildCommandBuffers(VulkanSwapChain* swapChainX,
 
         std::array<VkClearValue, 2> clearValues{};
         clearValues[0].color = { 0.0f, 0.0f, 0.15f, 1.0f };
+        //clearValues[0].color = { 240. / 255. , 188. / 255., 158. / 255., 1.0f };
+
         clearValues[1].depthStencil = { 1.0f, 0 };
 
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
